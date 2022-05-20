315340e211dc94c6caf063ee89ca9450301de181
aeonmerx <103615875+aeonmerx@users.noreply.github.com>
2022-05-20 15:33:03 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-Route::get('index', function () {
-    return ('la ruta funciona adecuadamente');
-});
 
 
